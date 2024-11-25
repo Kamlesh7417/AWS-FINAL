13c6470443952ec5b0492198e813584de9adcd12
Kamlesh7417 <51952452+Kamlesh7417@users.noreply.github.com>
2024-11-26 03:15:19 +0530

    Update ShipmentMap.tsx

--- src/components/shipping/ShipmentMap.tsx
+++ src/components/shipping/ShipmentMap.tsx
@@ -1,11 +1,57 @@
-import React, { useCallback } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Icon } from 'leaflet';
-import { Shipment } from '../../utils/mockData';
 import 'leaflet/dist/leaflet.css';
+
+export interface Shipment {
+  id: string;
+  origin: {
+    lat: number;
+    lng: number;
+    name: string;
+  };
+  destination: {
+    lat: number;
+    lng: number;
+    name: string;
+  };
+  status: string;
+}
 
 interface ShipmentMapProps {
   shipment: Shipment;
 }
 
-// Rest of the component implementation remains the same
+const ShipmentMap: React.FC<ShipmentMapProps> = ({ shipment }) => {
+  const markerIcon = new Icon({
+    iconUrl: '/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
+
+  const center = {
+    lat: (shipment.origin.lat + shipment.destination.lat) / 2,
+    lng: (shipment.origin.lng + shipment.destination.lng) / 2,
+  };
+
+  return (
+    <MapContainer
+      center={[center.lat, center.lng]}
+      zoom={4}
+      style={{ height: '400px', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[shipment.origin.lat, shipment.origin.lng]} icon={markerIcon}>
+        <Popup>{shipment.origin.name}</Popup>
+      </Marker>
+      <Marker position={[shipment.destination.lat, shipment.destination.lng]} icon={markerIcon}>
+        <Popup>{shipment.destination.name}</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default ShipmentMap;
