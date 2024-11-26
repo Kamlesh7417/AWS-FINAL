2342b313e2e78cf672c6bd2727f05290a723c1d4
Kamlesh7417 <51952452+Kamlesh7417@users.noreply.github.com>
2024-11-26 11:23:36 +0530

    Update PaymentModal.tsx

--- src/components/orders/PaymentModal.tsx
+++ src/components/orders/PaymentModal.tsx
@@ -5,8 +5,8 @@ import {
   CreditCardIcon
 } from '@heroicons/react/24/outline';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../../store';
-import { updateOrderStatusAndGenerateLabel } from '../../../store/slices/orderSlice';
+import { AppDispatch, RootState } from '../../store';
+import { updateOrderStatusAndGenerateLabel } from '../../store/slices/orderSlice';
 import confetti from 'canvas-confetti';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
