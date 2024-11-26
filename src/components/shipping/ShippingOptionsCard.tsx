683b63eb0d53181189f997a0be8907d24a92356c
Kamlesh7417 <51952452+Kamlesh7417@users.noreply.github.com>
2024-11-26 10:09:53 +0530

    Update ShippingOptionsCard.tsx

--- src/components/shipping/ShippingOptionsCard.tsx
+++ src/components/shipping/ShippingOptionsCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import { ClockIcon, SparklesIcon, StarIcon } from '@heroicons/react/24/outline';
 
