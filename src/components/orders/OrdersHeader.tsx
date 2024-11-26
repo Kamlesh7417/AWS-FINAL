109b311c50ac16a8c26b1ec1b8303dd2baf146b4
Kamlesh7417 <51952452+Kamlesh7417@users.noreply.github.com>
2024-11-26 09:46:39 +0530

    Update OrdersHeader.tsx

--- src/components/orders/OrdersHeader.tsx
+++ src/components/orders/OrdersHeader.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-interface OrdersHeaderProps {
-}
-
-const OrdersHeader: React.FC<OrdersHeaderProps> = ({ onRefresh }) => {
+const OrdersHeader: React.FC = () => {
   return (
     <div className="flex items-center justify-between">
       <motion.h1 
